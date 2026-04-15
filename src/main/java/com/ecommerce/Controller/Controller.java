910bf28c36887ec36e698a2aaa389b14804dbbe5
nikhil s achar <acharnikhil72@gmail.com>
2026-04-15 12:14:06 +0530

    Verdion 1

--- src/main/java/com/ecommerce/Controller/Controller.java
+++ src/main/java/com/ecommerce/Controller/Controller.java
@@ -2,10 +2,13 @@ package com.ecommerce.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.Database.Cart;
@@ -37,14 +40,34 @@ public class Controller {
     }
 
     @PostMapping("/AllProducts/getproduct/{id}/add_to_cart")
-    public Products addToCart(@PathVariable Long id) {
-        return c_service.FetchFromProduct(id);
+    public Products addToCart(@PathVariable Long id, @RequestParam(defaultValue = "1") int q) {
+        return c_service.FetchFromProduct(id, q);
     }
 
     @GetMapping("/getproduct/add_to_cart/buynow")
     public Cart calculateTotalPrice(Cart cart) {
         return c_service.calculateTotalPrice(cart);
 
+    }
+
+    @PutMapping("/update_product/{id}")
+    public Products updateProduct(@PathVariable Long id, @RequestBody Products updatedProduct, @RequestParam int cr) {
+        return c_service.updateProduct(id, updatedProduct, cr);
+    }
+
+    @DeleteMapping("/cancel_order")
+    public void delete() {
+        c_service.delete();
+    }
+
+    @GetMapping("/price_range")
+    public List<Products> ProductRange(@RequestParam double min, @RequestParam double max) {
+        return c_service.ProductRange(min, max);
+    }
+
+    @DeleteMapping("restart_db")
+    public void restartDB() {
+        c_service.restartDB();
     }
 
 }
